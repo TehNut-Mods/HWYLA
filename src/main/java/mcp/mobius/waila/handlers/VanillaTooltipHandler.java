bc8f47a62dbe32bd2ed24372e3375a8a55674900
InsomniaKitten <ava@designdevils.co.uk>
2016-10-27 16:17:11 +0100

    Revert to blue italics

--- src/main/java/mcp/mobius/waila/handlers/VanillaTooltipHandler.java
+++ src/main/java/mcp/mobius/waila/handlers/VanillaTooltipHandler.java
@@ -15,7 +15,7 @@ public class VanillaTooltipHandler {
     public static String metaDataThroughput = "%s:%d";
     public static String metaDataWrapper = String.format("\u00a77[%s]", metaDataThroughput);
 
-    public static String modNameWrapper = "\u00a79\u00a7o%s";
+    public static String modNameWrapper = "\u00A7o\u00A79%s";
 
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
